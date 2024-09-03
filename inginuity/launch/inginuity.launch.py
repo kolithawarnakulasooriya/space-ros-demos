4bd081289598e520e24d2b2b66f83b915d153b6c
Kolitha Warnakulasooriya <kolitha.warnakulasooriya@outlook.com>
2024-09-02 23:20:31 -0500

    update the IMU ros bridge topic

--- inginuity/launch/inginuity.launch.py
+++ inginuity/launch/inginuity.launch.py
@@ -53,7 +53,9 @@ def generate_launch_description():
             package='ros_gz_bridge',
             executable='parameter_bridge',
             arguments=[
-                '/clock@rosgraph_msgs/msg/Clock[ignition.msgs.Clock',
+                '/clock@rosgraph_msgs/msg/Clock[ignition.msgs.Clock]',
+                '/model/helicopter/odometry@nav_msgs/msg/Odometry@ignition.msgs.Odometry',
+                '/imu@sensor_msgs/msg/Imu@ignition.msgs.IMU',
             ],
             output='screen')
 
--- inginuity/src/propellors.py
+++ inginuity/src/propellors.py
@@ -1,34 +1,47 @@
 #!/usr/bin/env python3
-
 import rclpy
 import rclpy.logging
 from rclpy.node import Node
 from std_msgs.msg import Float64MultiArray
 from std_srvs.srv import SetBool,Empty
 from std_srvs.srv._set_bool import SetBool_Request, SetBool_Response
+from geometry_msgs.msg import Twist
+from collections import deque
 
 class PropellorNode(Node):
 
     def __init__(self):
         super().__init__('propellor_node')
         self.propellor_publisher = self.create_publisher(Float64MultiArray, '/propellor_controller/commands', 1)
-        self.timer_period = 1.0 
+        self.elev_publisher = self.create_publisher(Float64MultiArray, '/elevation_controller/commands', 1)
+        self.timer_period = 0.5 
         self.timer = None
-        self.motor_up_speed = 500.0
-        self.motor_down_speed = 500.0
+        self.motor_up_speed = 150.0
+        self.motor_down_speed = 250.0 + 100.0
+        self.twist_queue = deque()
 
         # Here define services
         self.create_service(SetBool, '/start_rotors', self.start_rotors)
         self.create_service(SetBool, '/stop_rotors', self.stop_rotors)
-        self.create_subscription(Float64MultiArray, '/rotor_speed', self.handle_speed_change, qos_profile=1)
+        self.sub_cmd_vel = self.create_subscription(msg_type=Twist, topic='/cmd_vel', callback=self.handle_cmd_vel, qos_profile=1)
+        self.sub_cmd_vel
 
-    def handle_speed_change(self, msg: Float64MultiArray):
-        self.get_logger().info(f"upper rotter: {msg.data[0]}, Lower Rotter: {msg.data[1]}")
-        self.motor_up_speed = msg.data[0]
-        self.motor_down_speed = msg.data[0]
+        self.create_timer(self.timer_period, self.timer_cmd_Vel)
+
+    def handle_cmd_vel(self, msg: Twist):
+        self.twist_queue.append(msg)
+            
+    def timer_cmd_Vel(self):
+        if len(self.twist_queue) <= 0:
+            return
+        msg: Twist = self.twist_queue.pop()
+        if(msg.linear.z > 0):
+            target_vel = Float64MultiArray()
+            target_vel.data = [msg.linear.z]
+            self.elev_publisher.publish(target_vel)
     
     def timer_callback(self):
-        self.publish_rotor_val([-self.motor_up_speed, self.motor_down_speed])
+        self.publish_rotor_val([self.motor_up_speed, -self.motor_down_speed])
 
     def publish_rotor_val(self, value):
         target_vel = Float64MultiArray()
@@ -41,7 +54,6 @@ class PropellorNode(Node):
             return response
         
         self.timer = self.create_timer(self.timer_period, self.timer_callback)
-
         return response
     
     def stop_rotors(self, request: SetBool_Request, response: SetBool_Response):
@@ -53,7 +65,6 @@ class PropellorNode(Node):
         self.timer.destroy()
         self.timer = None
         self.publish_rotor_val([0.0, 0.0])
-
         return response
             
 
